9e84d2e4eed40f97b00669f2012c9c64fb867c8f
Sergey Aleynikov <sergey.aleynikov@gmail.com>
2016-03-13 23:30:07 +0300

    add docs for namespace::clean::xs::all

--- lib/namespace/clean/xs/all.pm
+++ lib/namespace/clean/xs/all.pm
@@ -18,4 +18,26 @@ BEGIN {
 1;
 __END__
 
+=head1 NAME
+
+namespace::clean::xs::all - Use XS for namespace::clean globally
+
+=head1 SYNOPSIS
+
+    use namespace::clean::xs::all; # at the beginning of your application
+
+=head1 DESCRIPTION
+
+This module replaces L<namespace::clean> with L<namespace::clean::xs> globally,
+so you won't have to search-and-replace usage lines. All L<namespace::clean>
+unfinished calls will be finished by non-xs version.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2016 by Sergey Aleynikov
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.1 or,
+at your option, any later version of Perl 5 you may have available.
+
 =cut
